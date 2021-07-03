2895f394ab7c52183b704ecf118dabcfcbc2bab7
Evgeny Shevchenko <royalsalsk@gmail.com>
2021-07-03 13:56:26 +0300

    fix resFavorite

--- src/components/FavoriteWeather.jsx
+++ src/components/FavoriteWeather.jsx
@@ -7,21 +7,23 @@ function FavoriteWeather({favoriteCity, apiKey}) {
     const [resFavorite, setResFavorite] = useState([]);
 
     useEffect(() => {
-        favoriteCity
-            .map(city => axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
-                .then(({data}) => setResFavorite(data.main))
-            )
+        if (favoriteCity) {
+            favoriteCity
+                .map(city => axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+                    .then(({data}) => setResFavorite(data.main))
+                )
+        }
     }, [])
 
-    console.log(resFavorite)
+    console.log(favoriteCity)
 
     return (
         <div className="favoriteWeather">
-            <div className="favoriteWeather__content">
-                {resFavorite ? resFavorite.map((data, i) =>
-                    <FavoriteCard key={i + data} city={favoriteCity} {...data}/>
-                ) : resFavorite}
-            </div>
+            {/*<div className="favoriteWeather__content">*/}
+            {/*    {resFavorite ? resFavorite.map((data, i) =>*/}
+            {/*        <FavoriteCard key={i + data} city={favoriteCity} {...data}/>*/}
+            {/*    ) : resFavorite}*/}
+            {/*</div>*/}
         </div>
     )
 
